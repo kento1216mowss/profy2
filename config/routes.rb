8bbd9111783f277b0efda5afe805c0fc0fa4552d
EC2 Default User <ec2-user@ip-172-31-41-138.ap-northeast-1.compute.internal>
2020-03-31 08:46:21 +0000

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     root to: "top#index"
     resources :users, only: [:show, :edit, :update]
     resources :groups, only: [:show, :edit, :update]
+    resources :questions, only: [:create, :show]
+    resources :answers, only: [:new, :create]
+
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
